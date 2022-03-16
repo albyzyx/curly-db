c210a30d65e011fcd2c21cf80055ac2df3d8d7bb
albyzyx <albinarun24@gmail.com>
2022-03-17 00:50:54 +0530

    Fixed file sync bug

--- fsHelper.js
+++ fsHelper.js
@@ -1,30 +1,31 @@
-const fs = require("fs").promises;
+const fs = require("fs");
+
+// let path = null;
 
 const getJSON = (path) => {
-  const json = fs.readFile(
-    `${path ? `${path}/store.json` : "./store.json"}`,
-    "utf8",
-    (err) => {
-      console.log("Error in while getting json");
-      console.err(err);
-    }
-  );
-  // console.log(json);
-  return json;
+  if (fs.existsSync(`${path ? path : "store.json"}`)) {
+    const json = fs.readFileSync(
+      `${path ? `${path}/store.json` : "./store.json"}`,
+      "utf8",
+      (err) => {
+        console.log("Error in while getting json");
+        console.log(err);
+      }
+    );
+    // console.log(json);
+    return JSON.parse(json);
+  } else {
+    return {};
+  }
 };
 
 // getJSON();
 
-const setJSON = async (json) => {
-  const path = ".";
-  await fs.writeFile(
-    `${path ? `${path}/store.json` : "./store.json"}`,
-    JSON.stringify(json),
-    (err) => {
-      console.log("Error in while fetching json");
-      console.err(err);
-    }
-  );
+const setJSON = (json) => {
+  fs.writeFile("store.json", JSON.stringify(json), (err) => {
+    console.log("Error in while fetching json");
+    console.log(err);
+  });
 };
 
 // setJSON((json = { name: "test" }));
